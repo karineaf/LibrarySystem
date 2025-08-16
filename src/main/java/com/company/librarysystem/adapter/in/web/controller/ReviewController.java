ca9f87abf5cd1422caae2e8ba763708d11883809
Karine Antunes de Farias <50750300+karineaf@users.noreply.github.com>
2025-08-16 13:01:32 -0300

    adjusting url review

--- src/main/java/com/company/librarysystem/adapter/in/web/controller/ReviewController.java
+++ src/main/java/com/company/librarysystem/adapter/in/web/controller/ReviewController.java
@@ -6,15 +6,18 @@ import com.company.librarysystem.adapter.in.web.dto.mapper.ReviewDTOMapper;
 import com.company.librarysystem.application.service.ReviewService;
 import com.company.librarysystem.domain.model.Review;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
+import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE;
 import static org.springframework.http.ResponseEntity.*;
 
 
@@ -53,5 +56,57 @@ public class ReviewController {
         service.deleteReview(id);
         return noContent().build();
     }
+
+    @GetMapping("/search/by_user/{userId}")
+    public ResponseEntity<List<ReviewDTO>> findByUserId(@PathVariable Long userId) {
+        return ResponseEntity.ok(
+                service.getReviewByUserId(userId)
+                        .stream()
+                        .map(dtoMapper::toDTO)
+                        .toList()
+        );
+    }
+
+    @GetMapping("/search/by_book/{bookId}")
+    public ResponseEntity<List<ReviewDTO>> findByBookId(@PathVariable Long bookId) {
+        return ResponseEntity.ok(
+                service.getReviewByBookId(bookId)
+                        .stream()
+                        .map(dtoMapper::toDTO)
+                        .toList()
+        );
+    }
+
+    @GetMapping("/search/by_book_title")
+    public ResponseEntity<List<ReviewDTO>> findByBookTitle(@RequestParam("book_title") String bookTitle) {
+        return ResponseEntity.ok(
+                service.getReviewByBookTitle(bookTitle)
+                        .stream()
+                        .map(dtoMapper::toDTO)
+                        .toList()
+        );
+    }
+
+    @GetMapping("/search/by_rating/{rating}")
+    public ResponseEntity<List<ReviewDTO>> findByRating(@PathVariable Integer rating) {
+        return ResponseEntity.ok(
+                service.getReviewsByRating(rating)
+                        .stream()
+                        .map(dtoMapper::toDTO)
+                        .toList()
+        );
+    }
+
+    @GetMapping("/search/by_created_at/{createdAt}")
+    public ResponseEntity<List<ReviewDTO>> findByCreatedAt(
+            @PathVariable @DateTimeFormat(iso = DATE) LocalDate createdAt) {
+        return ResponseEntity.ok(
+                service.getReviewsByCreatedAt(createdAt)
+                        .stream()
+                        .map(dtoMapper::toDTO)
+                        .toList()
+        );
+    }
+
 }
 
